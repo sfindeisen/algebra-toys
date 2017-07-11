3a5a2be7d727469e4c847cf9b5e6ad25f9713226
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2017-07-11 22:43:26 +0200

    fibonacci

--- toys.hs
+++ toys.hs
@@ -1,3 +1,15 @@
+fibo 0 = 0
+fibo 1 = 1
+fibo n = snd (fib_pair n 0 1)
+    where
+        fib_pair k f1 f2 =
+            if (1 == k) then
+                (f1, f2)
+            else
+                fib_pair (k-1) f2 (f1+f2)
+
+-- map fibo [0..15]
+
 gen f start g n = reverse (xcalc start [])
     where
         xcalc a t =
